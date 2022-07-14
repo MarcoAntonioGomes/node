78c816c8f0f6f5df18c4e4d004265d440322f6ba
MarcoAntonioGomes <marcoantonio.gomes@yahoo.com.br>
2022-07-14 15:17:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Wesley')`
+connection.query(sql)
+
+
+const sqlNames = `SELECT name FROM people;`
+var names 
+var namesFormated = ''
+connection.query(sqlNames, function (error, results) {
+    if (error) {
+      console.log("Erro ao obter os nomes cadastrados!" + error) 
+    }
+    else {
+        results.forEach(formattNames)
+    }
+
+});
+connection.end()
+
+function formattNames(element) {
+  namesFormated = namesFormated + element.name + '<br> '
+}
+
+app.get('/', (req, res) => {
+    res.send('<h1>Full Cycle</h1> <br> Lista de Nomes: <br>' +  namesFormated)
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
